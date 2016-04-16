5d26b6a6b807726d0760754302dc9b2ec432309b
wulovecode <1129187334@qq.com>
2016-04-17 02:27:03 +0800

    入库toanado demo

---
+++ tornado_demo/hello_world.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__version__ = '1.0.0.0'
+
+"""
+@file:    hello_world.py
+@brief:   简介
+@details: 详细信息
+@author:  wxs
+@date:    2016-04-06
+"""
+
+import textwrap
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+
+class ReverseHandler(tornado.web.RequestHandler):
+    def get(self, input):
+        self.write(input[::-1])
+
+class WrapHandler(tornado.web.RequestHandler):
+    def post(self):
+        text = self.get_argument('text')
+        width = self.get_argument('width', 40)
+        self.write(textwrap.fill(text, int(width)))
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        handlers=[
+            (r"/reverse/(\w+)", ReverseHandler),
+            (r"/wrap", WrapHandler)
+        ]
+    )
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
